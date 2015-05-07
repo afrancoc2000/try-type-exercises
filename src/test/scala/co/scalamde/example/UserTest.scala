cf6cadc5df735038435f9c8aa00986947bdc86d2
afrancoc2000 <afrancoc2000@gmail.com>
2015-05-06 21:21:14 -0500

    Solución

--- src/test/scala/co/scalamde/example/UserTest.scala
+++ src/test/scala/co/scalamde/example/UserTest.scala
@@ -2,20 +2,34 @@ package co.scalamde.example
 
 import org.scalatest.FunSuite
 
+import scala.util.{Failure, Success}
+
 class UserTest extends FunSuite {
 
   test("Encuentra la edad del usuario 1") {
-    val edad: Int = ???
+    val usr = UsuarioRepository.findById(1)
+    val edad: Int = usr match{
+      case Success(u) => u.edad
+      case Failure(e) => 0
+    }
     assert(edad === 32)
   }
 
   test("Encuentra el genero del usuario 2") {
-    val genero: Option[String] = ???
+    val usr = UsuarioRepository.findById(2)
+    val genero: Option[String] = usr match{
+      case Success(u) => u.genero
+      case Failure(e) => None
+    }
     assert(genero === None)
   }
 
   test("Encuentra el genero del usuario 3") {
-    val genero: Option[String] = ???
+    val usr = UsuarioRepository.findById(3)
+    val genero: Option[String] = usr match{
+      case Success(u) => u.genero
+      case Failure(e) => None
+    }
     assert(genero === None)
   }
 }
